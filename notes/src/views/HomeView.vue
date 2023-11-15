7dfa32e41dbab07924b1eee7cc9a70f30419e35c
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2023-11-15 16:39:50 +0100

    project cleanup + added bootstrap

--- notes/src/views/HomeView.vue
+++ notes/src/views/HomeView.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Hayyyya notes" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld,
   },
